34e544a179dd7c12cd25be0ed79893be274a6173
LeozinRomeiro <leozinwork@gmail.com>
2023-08-15 07:38:16 -0300

    Adicionar arquivos de projeto.

---
+++ SaveAddress/SaveAddress/Pages/AbaFlyoutPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SaveAddress.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AbaFlyoutPage : ContentPage
+    {
+        public AbaFlyoutPage()
+        {
+            InitializeComponent();
+        }
+
+        private void botao_enderecos_Clicked(object sender, EventArgs e)
+        {
+            ((MainPage)Application.Current.MainPage).Navegar(new AddressPage());
+        }
+    }
+}
---
+++ SaveAddress/SaveAddress/Pages/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using SaveAddress.Pages;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SaveAddress.Pages
+{
+    public partial class MainPage : FlyoutPage
+    {
+        public AbaFlyoutPage flyoutPage;
+        public MainPage()
+        {
+            object value = InitializeComponent();
+            flyoutPage = new AbaFlyoutPage();
+            Flyout = flyoutPage;
+            Detail = new NavigationPage(new SobrePage());
+        }
+
+        public void Navegar(ContentPage page)
+        {
+            Detail = new NavigationPage(page);
+            IsPresented = false;
+        }
+    }
+}
