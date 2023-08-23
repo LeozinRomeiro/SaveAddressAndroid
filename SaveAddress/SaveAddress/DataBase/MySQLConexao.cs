f1e82d1a9e017a4c65fd47881fa647bf4cde0c0e
leozinromeiro <leozinwork@gmail.com>
2023-08-22 22:17:06 -0300

    Inicio

---
+++ SaveAddress/SaveAddress/DataBase/MySQLConexao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaveAddress.DataBase
+{
+    public class MySQLConexao
+    {
+        static string Connetion
+    }
+}
